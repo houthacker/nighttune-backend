947e8e2f65ac86584d95693155737275980124fb
houthacker <houthacker@pm.me>
2025-11-12 16:34:23 +0100

    Allow setting cookie on turnstile test

--- src/routes/turnstile.ts
+++ src/routes/turnstile.ts
@@ -9,6 +9,7 @@ import { getSession } from '../controllers/sessionController.js';
 
 const corsOptions: CorsOptions = {
     origin: process.env.NT_CORS_ALLOWED_ORIGINS?.split(',') || [],
+    credentials: true,
 };
 const router = Router();
 
@@ -20,11 +21,11 @@ router.post('/', cors(corsOptions), async (request: Request, response: Response)
     const ip = request.get('CF-Connecting-IP') || request.ip!;
     const validation = await validateTurnstile(request.body.token, ip);
 
-    const session = await getSession(request, response);
-    session.turnstileTestPassed = validation.success;
-    await session.save();
-
     if (validation.success) {
+        const session = await getSession(request, response);
+        session.turnstileTestPassed = validation.success;
+        await session.save();
+        
         response.status(200);
     } else {
         console.error('Invalid turnstile verification: ', validation['error-codes']);
