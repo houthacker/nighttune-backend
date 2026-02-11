1b0b8738f45a1965fe9a2c98ec9caa7edfe5fadb
houthacker <houthacker@pm.me>
2026-02-11 19:07:11 +0100

    Implement NightscoutApi.verify for v3
    To prevent extraneous network calls, generated JWTs are cached by url
    and access token. On a next call, the JWT is refreshed if it is valid
    for less than 5 seconds.

--- src/dao/nightscout/v3.ts
+++ src/dao/nightscout/v3.ts
@@ -1,35 +1,104 @@
-import { NightscoutApi } from '../../models/nightscout.js' 
+import { NightscoutApi } from '../../models/nightscout.js'
 import { NightscoutApiV1 } from './v1.js'
 
+import { jwtDecode, JwtPayload } from 'jwt-decode'
+
+import { HeadersInit } from 'node-fetch'
 import logger from '../../logger.js'
 
-const getJWT = async (url: URL, token: string): Promise<string> => {
-    const tokenRequestUrl = new URL(`/api/v2/authorization/request/${token}`, url)
-    const response = await fetch(tokenRequestUrl)
+type NightscoutAccessToken = string
 
-    if (response.ok) {
-        const body = await response.json() as { token: string }
-        return body.token
+class JWT {
+
+    private readonly decoded: JwtPayload & {
+        accessToken: string
     }
 
-    logger.warn(`JTW request failed for url [${url.href}]: returned HTTP status ${response.status}`)
-    return Promise.reject<string>(new Error('JWT request failed.'))
+    public readonly raw
+
+    constructor(jwt: string) {
+        this.decoded = jwtDecode(jwt)
+        this.raw = jwt
+    }
+
+    static async acquire(url: URL, accessToken: NightscoutAccessToken): Promise<JWT> {
+        const tokenRequestUrl = new URL(`/api/v2/authorization/request/${accessToken}`, url)
+        const response = await fetch(tokenRequestUrl)
+
+        if (response.ok) {
+            const body = await response.json() as { token: string }
+            return new JWT(body.token)
+        }
+
+        logger.warn(`Failed to get JTW for url [${url.href}]: returned HTTP status ${response.status}`)
+        return Promise.reject<JWT>(new Error('JWT request failed.'))
+    }
+
+    accessToken(): NightscoutAccessToken {
+        return this.decoded.accessToken
+    }
+
+    /**
+     * Returns whether this `JWT` is valid, optionally using the given
+     * safety margin. This allows checking if for example this token is
+     * valid for at least the next `x` seconds.
+     * 
+     * @param margin The margin in seconds to subtract from the actual expiry time. Defaults to 5 seconds.
+     */
+    isValid(margin: number = 5): boolean {
+        const now = Math.floor(Date.now() / 1000)
+        return this.decoded.exp! - Math.floor(margin) > now
+    }
 }
 
 export class NightscoutApiV3 extends NightscoutApiV1 implements NightscoutApi {
 
+    /**
+     * A cache of JTW tokens, indexed by their URL and Access Token.
+     */
+    private tokenCache: Map<{url: string, token: NightscoutAccessToken}, JWT>
+
     constructor() {
         super()
+
+        this.tokenCache = new Map()
     }
 
-    override async verify(url: URL, token?: string): Promise<boolean> {
+    private async acquireValidToken(url: URL, accessToken: NightscoutAccessToken): Promise<JWT> {
+        let jwt = this.tokenCache.get({url: url.href, token: accessToken})
+        if (jwt && jwt.isValid()) {
+            return jwt
+        }
+
+        jwt = await JWT.acquire(url, accessToken)
+        this.tokenCache.set({url: url.href, token: accessToken}, jwt)
+
+        return jwt
+    }
+
+    override async verify(url: URL, token?: NightscoutAccessToken): Promise<boolean> {
         const statusUrl = new URL('/api/v3/status', url)
-        const response = await fetch(statusUrl, {
-            headers: {
-                'Accept': 'application/json',
-                'Authorization': `Bearer ${await getJWT(url, token!)}`
+        const headers: HeadersInit = {
+            'Accept': 'application/json',
+        }
+
+        try {
+            if (token) {
+                const jwt = await this.acquireValidToken(url, token!)
+                headers['Authorization'] = `Bearer ${jwt.raw}`
             }
-        })
+
+            const response = await fetch(statusUrl, { headers })
+            if (response.ok) {
+                return true
+            }
+
+            logger.warn(`Verification of Nightscout API at '${url.href}' failed: HTTP ${response.status} (${response.statusText}) `)
+            return false
+        } catch (error: any) {
+            logger.error(`Verification of Nightscout API at '${url.href}' failed: \n${JSON.stringify(error)}`)
+        }
+
         return false
     }
 }
