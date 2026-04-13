093ce4298e9e04525845bfe108010f2cbde5f83d
houthacker <houthacker@pm.me>
2026-04-13 15:33:05 +0200

    Add distributed tracing to optional services

--- src/utils/optionalServiceUtil.ts
+++ src/utils/optionalServiceUtil.ts
@@ -9,30 +9,31 @@ function all(vars: any[]): boolean {
  * Scan the process environment to determine enabled optional services for the current runtime.
  */
 export function scanEnabledOptionalServices(): void {
-    const scannedServices = new Set<OptionalService>()
+    const _enabledServices = new Set<OptionalService>()
 
-    // Captcha service scanning
-    const captchaEnvVars: any[] = [process.env.NT_CAPTCHA_SITEKEY, process.env.NT_CAPTCHA_SECRET]
-    if (all(captchaEnvVars)) {
-        scannedServices.add(OptionalService.Captcha)
-    } else {
-        logger.debug('Disabling captcha service because the related environment variables are not configured.')
+    function scan(service: OptionalService, vars: any[]) {
+        if (all(vars)) {
+            _enabledServices.add(service)
+        } else {
+            logger.debug(`Disabling ${service} service because the related environment variables are not configured.`)
+        }
     }
 
-    // Mail service scanning
-    const mailEnvVars: any[] = [
+    // Scan optional services
+    scan(OptionalService.Captcha, [process.env.NT_CAPTCHA_SITEKEY, process.env.NT_CAPTCHA_SECRET])
+    scan(OptionalService.Sendmail, [
         process.env.NT_MAIL_APIKEY_PUBLIC, 
         process.env.NT_MAIL_APIKEY_PRIVATE, 
         process.env.NT_MAIL_SENDER_ADDRESS,
         process.env.NT_MAIL_SENDER_NAME,
-    ]
-    if (all(mailEnvVars)) {
-        scannedServices.add(OptionalService.Sendmail)
-    } else {
-        logger.debug('Disabling mailing service because the related environment variables are not configured.')
-    }
+    ])
+    scan(OptionalService.DistributedTracing, [
+        process.env.NT_DTRACE_SERVICE_NAME,
+        process.env.NT_DTRACE_URL,
+        process.env.NT_DLOG_URL,
+    ])
 
-    globalThis.enabledServices = scannedServices
+    globalThis.enabledServices = _enabledServices
 }
 
 /**
